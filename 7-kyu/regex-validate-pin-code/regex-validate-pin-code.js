82456aa219609676be442de2f7e26093c409f198
Chris <chrissdev.io@gmail.com>
2026-03-27 07:53:20 -0700

    Add solution - CodeHub

---
+++ 7-kyu/regex-validate-pin-code/regex-validate-pin-code.js
@@ -0,0 +1,16 @@
+function validatePIN(pin) {
+  if (pin.length !== 4 && pin.length !== 6) {
+    return false;
+  }
+​
+  for (let char of pin) {
+    if (char < '0' || char > '9') {
+      return false;
+    }
+  }
+​
+  return true;
+}
+​
+     
+​
